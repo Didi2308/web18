6ea6dd72c6c14a474f34896e5e3e8823d92ecd50
Dimitri Blard <dimitri.blard@viacesi.fr>
2019-05-07 13:56:49 +0200

    modification formulaire

--- includes/content.php
+++ includes/content.php
@@ -3,7 +3,7 @@
 $content = glob('./includes/*.inc.php');
 $page = isset($_GET['page']) ? $_GET['page'] : "";
 
-$page = './includes/' . $page . '.php';
+$page = './includes/' . $page . '.inc.php';
 
 if (in_array($page, $content)) {
     require $page;
--- includes/registration.inc.php
+++ includes/registration.inc.php
@@ -1,4 +1,39 @@
 <?php
 
-require_once 'frmRegistration.php';
+if (isset($_POST['validation'])) {
+    $nom = isset($_POST['nom']) ? $_POST['nom'] : "";
+    $prenom = isset($_POST['prenom']) ? $_POST['prenom'] : "";
+    $email = isset($_POST['email']) ? $_POST['email'] : "";
+    $mdp = isset($_POST['mdp']) ? $_POST['mdp'] : "";
+
+    $erreur = array();
+
+    if ($nom == "") array_push($erreur, "Veuillez saisir un nom");
+    if ($prenom == "") array_push($erreur, "Veuillez saisir un prénom");
+    if ($email == "") array_push($erreur, "Veuillez saisir un mail");
+    if ($mdp == "") array_push($erreur, "Veuillez saisir un mot de passe");
+
+    if(count($erreur)>0) {
+        $msgErreur = "<ul>";
+
+        for($i = 0 ; $i < count($erreur) ; $i++) {
+            $msgErreur .= "<li>" . $erreur[$i] . "</li>";
+        }
+
+        $msgErreur .= "</ul>";
+
+        echo $msgErreur; 
+
+        require_once 'frmRegistration.php';
+    }
+
+    else {
+        echo "Insertion en BDD";
+    }
+}
+    else {
+       require_once 'frmRegistration.php';
+ 
+    }
+
 
